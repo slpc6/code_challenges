0641991679197fa0876b24156fafc23a4b31b502
slpc6 <jmoralesgomez0106@gmail.com>
2026-01-02 17:29:51 -0500

    feat: introduction levels

---
+++ HackRank/Introduccion/division.py
@@ -0,0 +1,6 @@
+"""Division"""
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a // b, a / b, sep='\n')
---
+++ HackRank/Introduccion/loops.py
@@ -0,0 +1,5 @@
+"""Loops"""
+
+if __name__ == '__main__':
+    n = int(input())
+    print(*(i**2 for i in range(n)), sep='\n')
