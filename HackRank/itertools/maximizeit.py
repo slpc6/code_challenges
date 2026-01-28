3ac95d81cb95f9d9e61cdbaca46f25908ec294e0
slpc6 <jmoralesgomez0106@gmail.com>
2026-01-28 09:48:24 -0500

    feat: linter errors

--- HackRank/itertools/maximizeit.py
+++ HackRank/itertools/maximizeit.py
@@ -5,13 +5,13 @@ from itertools import product
 K, M = map(int,input().split())
 lists = []
 for i in range(K):
-    L = list(map(int, input().split()))
-    L.pop(0)
-    lists.append(L)
+    l = list(map(int, input().split()))
+    l.pop(0)
+    lists.append(l)
 
-count = 0
+max_mod = 0
 for com in product(*lists):
     total = sum(pow(x, 2) for x in com) % M
-    if total > count:
-        count = total
-print(count)
+    if total > max_mod:
+        max_mod = total
+print(max_mod)
